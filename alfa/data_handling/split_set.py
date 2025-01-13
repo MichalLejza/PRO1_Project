45a10a36ff72734d373d814525537de414051524
Michał <m01lejza@gmail.com>
2025-01-13 23:04:13 +0100

    ver 1.6

---
+++ alfa/data_handling/split_set.py
@@ -0,0 +1,38 @@
+from torch.utils.data import Dataset, DataLoader, TensorDataset
+from torchvision import transforms
+from .dataset import GesturesDataset
+
+
+class SplitSet(Dataset):
+    def __init__(self, dataset: GesturesDataset, train: bool = True, test: bool = False,
+                 transform: transforms = None, target_transform: transforms = None):
+        self.dataset = dataset
+
+    def __len__(self) -> int:
+        """
+
+        :return:
+        """
+        pass
+
+    def __getitem__(self, item) -> tuple:
+        """
+
+        :param item:
+        :return:
+        """
+        pass
+
+    def get_data_loader(self) ->  DataLoader:
+        """
+
+        :return:
+        """
+        pass
+
+    def print_dataset_info(self) -> None:
+        """
+
+        :return:
+        """
+        pass
