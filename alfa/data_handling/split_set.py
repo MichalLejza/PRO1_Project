8a772d1e820ad4af9786995fcae4023db259b830
Michał <m01lejza@gmail.com>
2025-01-14 20:38:25 +0100

    ver 1.5

--- alfa/data_handling/split_set.py
+++ alfa/data_handling/split_set.py
@@ -1,4 +1,4 @@
-from torch.utils.data import Dataset, DataLoader, TensorDataset
+from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from .dataset import GesturesDataset
 
