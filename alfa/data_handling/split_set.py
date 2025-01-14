a37a8674ab0596b545b6d3c940888f9c0c493e01
Michał <m01lejza@gmail.com>
2025-01-14 22:56:48 +0100

    split_set almost completed

--- alfa/data_handling/split_set.py
+++ alfa/data_handling/split_set.py
@@ -1,16 +1,19 @@
-from torch.utils.data import Dataset, DataLoader
+import torch
+from torch.utils.data import Dataset, DataLoader, TensorDataset
 from torchvision import transforms
 from .dataset import GesturesDataset
 
 
 class SplitSet(Dataset):
-    def __init__(self, gestureDataset: GesturesDataset, train: bool = True, test: bool = False,
+    def __init__(self, gestureDataset: GesturesDataset, train: bool = False, test: bool = False,
                  transform: transforms = None, target_transform: transforms = None):
-        self.dataset = gestureDataset
         if train:
             self.dataset = gestureDataset.train_data
         elif test:
             self.dataset = gestureDataset.test_data
+        self.categories_map = gestureDataset.categories_map
+        self.filter_dataset()
+        self.data, self.target = self._split_data()
         self.transform = transform
         self.target_transform = target_transform
 
@@ -27,25 +30,64 @@ class SplitSet(Dataset):
         :param item:
         :return:
         """
-        pass
+        data = self.data[item]
+        target = self.target[item]
+        if self.transform:
+            data = self.transform(data)
+        return data, target
+
+    def get_gesture(self, index: int) -> str:
+        """
+
+        :param index:
+        :return:
+        """
+        for category, value in self.categories_map.items():
+            if value == index:
+                return category
+        # to do exception
+
+    def filter_dataset(self) -> None:
+        """
+
+        :return:
+        """
+        for row in self.dataset:
+            if row[-2] == 0:
+                self.dataset.remove(row)
 
     def _split_data(self) -> tuple:
         """
 
         :return:
         """
-        pass
+        data: list[list[float]] = []
+        targets: list[int] = []
+
+        for row in self.dataset:
+            data.append(row[1:-2])
+            targets.append(self.categories_map[row[-1]])
+
+        return data, targets
 
     def get_data_loader(self) ->  DataLoader:
         """
 
         :return:
         """
-        pass
+        tensor_data = torch.Tensor(self.data)
+        tensor_target = torch.Tensor(self.target)
+        dataset = TensorDataset(tensor_data, tensor_target)
+        return DataLoader(dataset, batch_size=32, shuffle=True)
+
+
 
     def print_dataset_info(self) -> None:
         """
 
         :return: None
         """
-        pass
+        print('Zbiór Danych: Gramatical facial Expression')
+        print(f'Wielkość zbioru danych: {len(self.data)}')
+        print(f'Ilość punktów: {len(self.data[0])}')
+        print(f'Ilość kategorii w zbiorze: {len(self.categories_map.items())}')
